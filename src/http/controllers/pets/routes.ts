d04c286308a1cc454059e84e735ea13599cda843
Ítalo Braga <itojk2@hotmail.com>
2023-05-10 01:22:51 -0300

    feat: authenticate validation

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,3 +1,4 @@
+import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
 import fastifyMulter from "fastify-multer";
 import { create } from "./create";
@@ -5,5 +6,7 @@ import { create } from "./create";
 const multer = fastifyMulter();
 
 export async function petsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", verifyJwt);
+
   app.post("/pets", { preHandler: multer.array("files", 4) }, create);
 }
